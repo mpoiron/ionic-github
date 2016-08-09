28d1ed17ce7f2980dee6d2b0729d06f852dbf8f9
Maxime Poiron <mpoiron@expertime.com>
2016-08-09 15:55:46 +0200

    First commit

---
+++ app/pages/home/home.ts
@@ -0,0 +1,24 @@
+import {Component} from '@angular/core';
+import {GithubProvider} from '../../providers/github/github';
+
+@Component({
+  templateUrl: 'build/pages/home/home.html',
+  providers: [GithubProvider]
+})
+export class HomePage {
+  public foundRepos;
+  public username;
+
+  constructor(private github: GithubProvider) {
+  }
+
+  getRepos() {
+    this.github.getRepoitories(this.username).subscribe(
+      data => {
+        this.foundRepos = data.json();
+      },
+      err => console.error(err),
+      () => console.log('Finished getting repositories')
+    );
+  }
+}
---
+++ app/providers/github/github.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class GithubProvider {
+
+  constructor(private http: Http) {}
+
+  getRepoitories(username) {
+    let repos = this.http.get(`https://api.github.com/users/${username}/repos`);
+    return repos;
+  }
+}
+
